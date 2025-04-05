55757d86768f523f64b8c0d581e0913d40676315
jaisatyaabhiram <abhiramjaisatya@gmail.com>
2025-04-05 19:49:16 +0530

    some changes made

--- market.py
+++ market.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Item(db.model):
+    name = db.Column(db.String(length=30))
 
 @app.route('/')
 @app.route('/home')
